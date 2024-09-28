20bbb48e10795f98671e218a2e947276e96abf5f
Michau <Michau@DESKTOP-53TJ0BT>
2024-09-28 17:50:44 +0200

    trying harder

--- src/app/periodic-table/periodic-table.component.ts
+++ src/app/periodic-table/periodic-table.component.ts
@@ -1,25 +1,15 @@
 import { Component, OnInit } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
-import { debounceTime, EMPTY, Observable, Subject, map } from 'rxjs';
+import { debounceTime, EMPTY, Observable, Subject, map, throwError, tap } from 'rxjs';
 import { CommonModule } from '@angular/common';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatDialogModule } from '@angular/material/dialog';
 import { FormsModule } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
-import { EditElementDialogComponent } from '../edit-element-dialog/edit-element-dialog.component';
 import { APIService } from '../services/api.service';
-
-
-// Define the PeriodicElement interface
-interface PeriodicElement {
-  position: number;
-  name: string;
-  weight: number;
-  symbol: string;
-  isHighlighted?: boolean;
-}
-
+import { PeriodicElement, PeriodicElementComponent } from "../periodic-element/periodic-element.component";
+import { MatGridListModule } from '@angular/material/grid-list';
 
 @Component({
   selector: 'app-root',
@@ -32,157 +22,85 @@ interface PeriodicElement {
     MatInputModule,
     MatDialogModule,
     FormsModule,
-    MatButtonModule
+    MatButtonModule,
+    MatGridListModule,
+    PeriodicElementComponent
   ],
 })
-
 export class PeriodicTableComponent implements OnInit {
-  elements$: Observable<any>;
-  elementsTable: Array<Array<PeriodicElement | null>> = [];
-  lanthanidesActinidesTable: Array<Array<PeriodicElement | null>> = [];
+  elements$: Observable<PeriodicElement[]>;
+  elementsTable: Array<Array<PeriodicElement>>;
   filterText: string = '';
   private filterSubject = new Subject<string>();
 
-  dataLoading = true;
-
-  constructor(private dialog: MatDialog,
-    private api: APIService,
-  ) {
+  constructor(private dialog: MatDialog, private api: APIService) {
     this.elements$ = EMPTY;
-    // Debounce the filter input with a 2-second delay
-    this.filterSubject.pipe(debounceTime(2000)).subscribe((filterValue) => {
-      this.applyFilter(filterValue);
-    });
+    this.elementsTable = Array.from({ length: 10 }, () => Array(19).fill({ empty: true }));
+    this.PreloadTable();
+  }
+  PreloadTable() {
+    PERIODIC_TABLE_MAP.forEach((value,key) =>{
+      this.elementsTable[value[0]][value[1]].position = key;
+    })
   }
 
   ngOnInit(): void {
     this.elements$ = this.api.getData();
-    this.elementsTable = Array.from({ length: 7 }, () => Array(18).fill(null));
-    this.lanthanidesActinidesTable = Array.from({ length: 2 }, () => Array(14).fill(null));
-
-    // Populate the main periodic table
     this.populateTable();
-    
-    // Handle filtering with debouncing
-    this.filterSubject.pipe(debounceTime(2000)).subscribe(value => {
-      this.filterText = value;
-      this.filterElements();
-    });
   }
 
   populateTable() {
-    this.elements$.subscribe((elements: PeriodicElement[]) => {
-      for (const element of elements) {
-        const position = [Math.floor((element.position - 1) / 18), (element.position - 1) % 18];
-        this.elementsTable[position[0]][position[1]] = { ...element, isHighlighted: false };
-      }
-    });
-  }
-  
-  filterElements() {
-    const searchText = this.filterText.toLowerCase();
-
-    // Clear highlights if filter is empty
-    const clearHighlight = searchText === '';
-
-    // Highlight elements in the main table
-    for (const row of this.elementsTable) {
-      for (const element of row) {
-        if (element) {
-          element.isHighlighted = !clearHighlight && element.name.toLowerCase().includes(searchText);
-        }
-      }
-    }
-
-    // Highlight elements in the lanthanides and actinides table (if applicable)
-    for (const row of this.lanthanidesActinidesTable) {
-      for (const element of row) {
-        if (element) {
-          element.isHighlighted = !clearHighlight && element.name.toLowerCase().includes(searchText);
-        }
-      }
-    }
-  }
-
-  openEditDialog(element: PeriodicElement): void {
-    const dialogRef = this.dialog.open(EditElementDialogComponent, {
-      data: element,
-    });
-
-    dialogRef.afterClosed().subscribe((result) => {
-      if (result) {
-        // You can handle the edited result here without mutating the original elements
-        // For example, just log or process the result as needed
-      }
-    });
-  }
-
-  applyFilter(filterValue: string): void {
-    const lowerCaseFilter = filterValue.toLowerCase();
-    
-    // Transform the elements$ observable using the map operator
-    this.elements$ = this.elements$.pipe(
-      map((elements: PeriodicElement[]) => 
-        elements.map((el) => ({
-          ...el,
-          // Highlighting is only for display purposes
-          isHighlighted: lowerCaseFilter
-            ? el.name.toLowerCase().includes(lowerCaseFilter) || 
-              el.symbol.toLowerCase().includes(lowerCaseFilter)
-            : false,
-        }))
-      )
-    );
-  }
-
-  onFilter(event: Event): void {
-    const inputValue = (event.target as HTMLInputElement).value;
-    this.filterSubject.next(inputValue); 
+    this.api.getData().pipe(
+      tap((elements:PeriodicElement[])=>{
+        elements.forEach((element)=>{
+          console.log('popTab: %s',element.name);
+          if(element.position!=null){
+            const coords = PERIODIC_TABLE_MAP.get(element.position);
+            if(coords){
+              this.elementsTable[coords[0]][coords[1]] = element;
+              console.log('Current element: %s',element.name);
+            }
+          }
+        })
+      })).subscribe();
   }
 }
-const MAIN_PERIODIC_TABLE_MAP: { [key: number]: [number, number] } = {
-  1: [0, 0], 2: [0, 17], 3: [1, 0], 4: [1, 1], 5: [1, 12],  
-  6: [1, 13], 7: [1, 14], 8: [1, 15], 9: [1, 16], 10: [1, 17],
-  11: [2, 0], 12: [2, 1], 13: [2, 12], 14: [2, 13], 15: [2, 14], 
-  16: [2, 15], 17: [2, 16], 18: [2, 17], 19: [3, 0], 20: [3, 1], 
-  21: [3, 2], 22: [3, 3], 23: [3, 4], 24: [3, 5], 25: [3, 6], 
-  26: [3, 7], 27: [3, 8], 28: [3, 9], 29: [3, 10], 30: [3, 11], 
-  31: [4, 0], 32: [4, 1], 33: [4, 2], 34: [4, 3], 35: [4, 4], 
-  36: [4, 5], 37: [4, 6], 38: [4, 7], 39: [4, 8], 40: [4, 9],  
-  41: [4, 10], 42: [4, 11], 43: [4, 12], 44: [4, 13], 45: [4, 14], 
-  46: [4, 15], 47: [4, 16], 48: [4, 17], 49: [5, 0], 50: [5, 1],  
-  51: [5, 2], 52: [5, 3], 53: [5, 4], 54: [5, 5], 55: [5, 6],  
-  56: [5, 7], 
-};
-const LANTHANIDES_ACTINIDES_MAP: { [key: number]: [number, number] } = {
-  57: [0, 0],  
-  58: [0, 1],  
-  59: [0, 2],  
-  60: [0, 3],  
-  61: [0, 4],  
-  62: [0, 5], 
-  63: [0, 6], 
-  64: [0, 7],  
-  65: [0, 8],  
-  66: [0, 9],  
-  67: [0, 10], 
-  68: [0, 11], 
-  69: [0, 12], 
-  70: [0, 13], 
-  71: [0, 14], 
-  89: [1, 0],  
-  90: [1, 1],  
-  91: [1, 2],  
-  92: [1, 3],  
-  93: [1, 4],  
-  94: [1, 5],  
-  95: [1, 6],  
-  96: [1, 7],  
-  97: [1, 8],  
-  98: [1, 9],  
-  99: [1, 10], 
-  100: [1, 11], 
-  101: [1, 12], 
-  102: [1, 13], 
-  103: [1, 14], 
-};
+
+const PERIODIC_TABLE_MAP = new Map<number, [number, number]>([
+  // Row 0 (Hydrogen and Helium)
+  [1, [0, 0]], [2, [0, 17]],
+
+  // Row 1 (Lithium to Neon)
+  [3, [1, 0]], [4, [1, 1]], [5, [1, 12]], [6, [1, 13]], [7, [1, 14]], [8, [1, 15]], [9, [1, 16]], [10, [1, 17]],
+
+  // Row 2 (Sodium to Argon)
+  [11, [2, 0]], [12, [2, 1]], [13, [2, 12]], [14, [2, 13]], [15, [2, 14]], [16, [2, 15]], [17, [2, 16]], [18, [2, 17]],
+
+  // Row 3 (Potassium to Krypton)
+  [19, [3, 0]], [20, [3, 1]], [21, [3, 2]], [22, [3, 3]], [23, [3, 4]], [24, [3, 5]], [25, [3, 6]], 
+  [26, [3, 7]], [27, [3, 8]], [28, [3, 9]], [29, [3, 10]], [30, [3, 11]], [31, [3, 12]], [32, [3, 13]], 
+  [33, [3, 14]], [34, [3, 15]], [35, [3, 16]], [36, [3, 17]],
+
+  // Row 4 (Rubidium to Xenon)
+  [37, [4, 0]], [38, [4, 1]], [39, [4, 2]], [40, [4, 3]], [41, [4, 4]], [42, [4, 5]], [43, [4, 6]], 
+  [44, [4, 7]], [45, [4, 8]], [46, [4, 9]], [47, [4, 10]], [48, [4, 11]], [49, [4, 12]], [50, [4, 13]], 
+  [51, [4, 14]], [52, [4, 15]], [53, [4, 16]], [54, [4, 17]],
+
+  // Row 5 (Cesium to Radon)
+  [55, [5, 0]], [56, [5, 1]], [57, [5, 2]], [72, [5, 3]], [73, [5, 4]], [74, [5, 5]], [75, [5, 6]], 
+  [76, [5, 7]], [77, [5, 8]], [78, [5, 9]], [79, [5, 10]], [80, [5, 11]], [81, [5, 12]], [82, [5, 13]], 
+  [83, [5, 14]], [84, [5, 15]], [85, [5, 16]], [86, [5, 17]],
+
+  // Row 6 (Francium to Oganesson)
+  [87, [6, 0]], [88, [6, 1]], [89, [6, 2]], [104, [6, 3]], [105, [6, 4]], [106, [6, 5]], [107, [6, 6]], 
+  [108, [6, 7]], [109, [6, 8]], [110, [6, 9]], [111, [6, 10]], [112, [6, 11]], [113, [6, 12]], [114, [6, 13]], 
+  [115, [6, 14]], [116, [6, 15]], [117, [6, 16]], [118, [6, 17]],
+
+  // Lanthanides (Row 8)
+  [58, [8, 3]], [59, [8, 4]], [60, [8, 5]], [61, [8, 6]], [62, [8, 7]], [63, [8, 8]], [64, [8, 9]], 
+  [65, [8, 10]], [66, [8, 11]], [67, [8, 12]], [68, [8, 13]], [69, [8, 14]], [70, [8, 15]], [71, [8, 16]],
+
+  // Actinides (Row 9)
+  [90, [9, 3]], [91, [9, 4]], [92, [9, 5]], [93, [9, 6]], [94, [9, 7]], [95, [9, 8]], [96, [9, 9]], 
+  [97, [9, 10]], [98, [9, 11]], [99, [9, 12]], [100, [9, 13]], [101, [9, 14]], [102, [9, 15]], [103, [9, 16]]
+]);
